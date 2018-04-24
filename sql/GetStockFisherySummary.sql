0738567727de9e10652f6fa3ec2387f1fdfd62f2
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2018-04-24 16:43:37 -0700

    remove tag code
    Now the query will aggregate to stock and not specific tag codes of the
    stock

--- sql/GetStockFisherySummary.sql
+++ sql/GetStockFisherySummary.sql
@@ -1,5 +1,4 @@
 SELECT SpeciesStock.Description AS Stock_Name, 
-       WireTagCode.TagCode, 
 	   Year(CWDBRecovery.RecoveryDate) AS Recovery_Year, 
 	   Month(CWDBRecovery.RecoveryDate) AS Recovery_Month, 
 	   CFileFishery.Id AS CFileFishery_Id, 
@@ -13,13 +12,11 @@ FROM ((Fishery
 		INNER JOIN CFileFishery ON FisheryCFileFishery.CFileFishery = CFileFishery.Id
 WHERE Year(CWDBRecovery.RecoveryDate)>= 2009
 GROUP BY SpeciesStock.Description, 
-	     WireTagCode.TagCode, 
 		 Year(CWDBRecovery.RecoveryDate), 
 		 Month(CWDBRecovery.RecoveryDate), 
 		 CFileFishery.Id, 
 		 CFileFishery.Description
-ORDER BY SpeciesStock.Description, 
-         WireTagCode.TagCode, 
+ORDER BY SpeciesStock.Description,
 		 Year(CWDBRecovery.RecoveryDate), 
 		 Month(CWDBRecovery.RecoveryDate), 
 		 CFileFishery.Id, 
