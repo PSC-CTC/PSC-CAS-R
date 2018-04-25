84b675c8c3a4889a5a9d783120a7c5f0daaa2961
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2018-04-25 09:59:17 -0700

    Added release clip status, observed total, expanded total, and age

--- sql/GetStockFisherySummary.sql
+++ sql/GetStockFisherySummary.sql
@@ -2,8 +2,14 @@ SELECT SpeciesStock.Description AS Stock_Name,
 	   Year(CWDBRecovery.RecoveryDate) AS Recovery_Year, 
 	   Month(CWDBRecovery.RecoveryDate) AS Recovery_Month, 
 	   CFileFishery.Id AS CFileFishery_Id, 
-	   CFileFishery.Description as CFileFishery_Name, 
-	   Sum(CWDBRecovery.AdjustedEstimatedNumber) AS SumOfAdjustedEstimatedNumber
+	   CFileFishery.Description as CFileFishery_Name,
+	   Age,
+	   switch(left(CWTMark1, 1) = "5", "Adclipped", left(CWTMark1, 1) = "0", "Unclipped") as Release_Status,
+	   Count(RecoveryId) as ObservedTotal,
+	   Sum(CWDBRecovery.AdjustedEstimatedNumber) AS SumOfAdjustedEstimatedNumber,
+	   Sum(CWDBRecovery.AdjustedEstimatedNumber *  
+	        ((Nz(CWTMark1Count, 0) + Nz(CWTMark2Count, 0) + Nz(NonCWTMark1Count, 0) + Nz(NonCWTMark2Count, 0)) /
+			 (Nz(CWTMark1Count, 0) + Nz(CWTMark2Count, 0)))) as Total_Expanded
 FROM ((Fishery 
 		INNER JOIN ((CWDBRecovery
 		INNER JOIN WireTagCode ON CWDBRecovery.TagCode = WireTagCode.TagCode)
@@ -14,10 +20,14 @@ WHERE Year(CWDBRecovery.RecoveryDate)>= 2009
 GROUP BY SpeciesStock.Description, 
 		 Year(CWDBRecovery.RecoveryDate), 
 		 Month(CWDBRecovery.RecoveryDate), 
+		 Age,
 		 CFileFishery.Id, 
-		 CFileFishery.Description
+		 CFileFishery.Description,
+		 switch(left(CWTMark1, 1) = "5", "Adclipped", left(CWTMark1, 1) = "0", "Unclipped")
 ORDER BY SpeciesStock.Description,
 		 Year(CWDBRecovery.RecoveryDate), 
 		 Month(CWDBRecovery.RecoveryDate), 
 		 CFileFishery.Id, 
-		 CFileFishery.Description;
+		 CFileFishery.Description,
+		 Age,
+		 switch(left(CWTMark1, 1) = "5", "Adclipped", left(CWTMark1, 1) = "0", "Unclipped");
